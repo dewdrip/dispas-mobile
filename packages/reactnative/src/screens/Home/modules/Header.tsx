1d65b0503cff5ef5d9a60ca18c608576ddf83437
Valentine Orga <valentineorga@gmail.com>
2025-03-08 09:58:34 +0100

    refactor: display network name

--- packages/reactnative/src/screens/Home/modules/Header.tsx
+++ packages/reactnative/src/screens/Home/modules/Header.tsx
@@ -1,19 +1,25 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { Pressable, StyleSheet, View } from 'react-native';
+import { Text } from 'react-native-paper';
 // @ts-ignore
 import Ionicons from 'react-native-vector-icons/dist/Ionicons';
 import Logo from '../../../components/Logo';
+import { useNetwork } from '../../../hooks/scaffold-eth';
+import globalStyles from '../../../styles/globalStyles';
 import { FONT_SIZE } from '../../../utils/styles';
 
 type Props = {};
 
 export default function Header({}: Props) {
   const navigation = useNavigation();
+  const network = useNetwork();
+
   return (
     <View style={styles.container}>
       <Logo size={FONT_SIZE.xl * 2.5} />
       <View style={styles.icons}>
+        <Text style={styles.network}>{network.name}</Text>
         <Pressable onPress={() => navigation.navigate('Wallet')}>
           <Ionicons
             name="wallet-outline"
@@ -41,6 +47,11 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingVertical: 16,
     paddingHorizontal: 10
+  },
+  network: {
+    ...globalStyles.text,
+    fontSize: FONT_SIZE.md,
+    color: '#555'
   },
   icons: {
     flexDirection: 'row',
